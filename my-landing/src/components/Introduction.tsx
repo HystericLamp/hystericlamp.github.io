af9d1480ad027beba95f7bf12e575a45cab71009
HystericLamp <159586448+HystericLamp@users.noreply.github.com>
2025-06-23 15:54:53 -0600

    Update Introduction.tsx

--- my-landing/src/components/Introduction.tsx
+++ my-landing/src/components/Introduction.tsx
@@ -7,7 +7,7 @@ const Introduction: React.FC = () => {
         <h2 className="text-4xl font-semibold mb-4 font-poppins gradient-light-text
                        dark:font-bold dark:gradient-dark-text">Hello, I'm Brian Cruz!</h2>
         <p className="text-xl max-w-xl mx-auto leading-relaxed">
-          I'm a Software Developer with some experience in Software QA, passionate about passionate about building robust, maintainable solutions through clean code and thorough testing.
+          I'm a Software Developer with some experience in Software QA, passionate about building robust, maintainable solutions through clean code and thorough testing.
           Focused on building reliable, high-quality applications that meet user needs and find real-world use.
         </p>
       </div>
